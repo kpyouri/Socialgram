135e8ea116aae4a23616738c4c93d1dac8026b71
Esther <esther.youri.shin@gmail.com>
2018-10-10 21:56:07 -0700

    added additional post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -27,4 +27,25 @@ const Post = require('../../models/Post');
     }
 );
 
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/', (req, res) => {
+    Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopostsfound: 'No posts found'}));
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+        res.status(404).json({ nopostfound: 'No post found with that ID '})
+        );
+});
+
 module.exports = router;
