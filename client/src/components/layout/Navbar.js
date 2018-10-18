7d868278e7d9238d86b8d7489df4b0bbda63a64d
Esther <esther.youri.shin@gmail.com>
2018-10-18 08:36:00 -0700

    fixed nav bar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,22 +3,33 @@ import React, { Component } from 'react'
 class Navbar extends Component {
   render() {
     return (
-<div className="landing">
-    <div className="dark-overlay landing-inner text-light">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <h1 className="display-3 mb-4">Developer Connector
-            </h1>
-            <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
-            <hr />
-            <a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
-            <a href="login.html" className="btn btn-lg btn-light">Login</a>
-          </div>
-        </div>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+    <div class="container">
+      <a class="navbar-brand" href="landing.html">DevConnector</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="mobile-nav">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="profiles.html"> Developers
+            </a>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="register.html">Sign Up</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.html">Login</a>
+          </li>
+        </ul>
       </div>
     </div>
-  </div>
+  </nav>
+
     )
   }
 }
