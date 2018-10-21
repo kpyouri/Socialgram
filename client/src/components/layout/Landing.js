62576b65266a7dcf9dde9bcccfd1832a5ef80265
Esther <esther.youri.shin@gmail.com>
2018-10-20 20:44:28 -0700

    changed the styles of landing, navbar and footer

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Landing extends Component {
   render() {
     return (
-<div className="landing">
+<div className="landing" >
     <div className="dark-overlay landing-inner text-light">
       <div className="container">
         <div className="row">
