042220d1769a1f939cafeb9267bd806cebb3798d
Rebekah <rebmid@gmail.com>
2018-10-29 22:22:48 -0700

    Added functionality so could get past login to profile

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,16 +1,60 @@
-import {GET_ERRORS} from './types';
+import {GET_ERRORS, SET_CURRENT_USER} from './types';
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
 
 //Register User
-export const registeruser = (userData) => dispatch => {
+export const registeruser = (userData, history) => dispatch => {
 
-axios
+    axios
     .post('/api/users/register', userData)
-   .then(res => console.log(res.data))
-   .catch(err => 
-    dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-    })
-);
+    .then(res => history.push('/login'))
+    .catch(err => 
+      dispatch ({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+
 }
+
+//Login User - get user token
+export const loginUser = (userData) => dispatch => {
+
+  axios
+  .post('/api/users/login', userData)
+  .then(res => {
+    //save to localstorage
+    const {token} = res.data//set token to ls
+    localStorage.setItem('jwtToken', token);
+    //Set token to auth header
+    setAuthToken(token);
+    //Decode token to get user data
+    const decoded = jwt_decode(token);
+    //SET current user
+    dispatch(setCurrentUser(decoded));
+
+  })
+  .catch(err => 
+    dispatch ({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  );
+}
+
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem('jwtToken');
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -47,14 +47,13 @@ class Register extends Component {
     const {errors} = this.state;
 
 
-
     return (
       <div className="register">
       <div className="container">
         <div className="row">
           <div className="col-md-8 m-auto">
             <h1 className="display-4 text-center">Sign Up</h1>
-            <p className="lead text-center">Create your Socialgram account</p>
+            <p className="lead text-center">Create your DevConnector account</p>
             <form noValidate onSubmit={this.onSubmit}>
               <div className="form-group">
                 <input type="text" 
