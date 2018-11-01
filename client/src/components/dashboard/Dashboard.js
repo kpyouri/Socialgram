501da06c349e167f18d30f7080906fe46074bb9c
Esther <esther.youri.shin@gmail.com>
2018-10-31 17:51:06 -0700

    PRed the code and made changes in css and dashboard.js file

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Posts from '../posts/Posts';
 
 const ProfileActions = () => {
   return (
-    <div className="btn-group mb-4" role="group">
+    <div role="group">
+    <div className="horizontal">
       <Link to="/edit-profile" className="btn btn-light">
         <i className="fas fa-user-circle text-info mr-1" /> Edit Profile
       </Link>
-      <Link to="/add-experience" className="btn btn-light">
+      {/* <Link to="/add-experience" className="btn btn-light">
         <i className="fab fa-black-tie text-info mr-1" />
         Add Experience
-      </Link>
+      </Link> */}
       
+    </div>
+    <Posts/>
     </div>
   );
 };
