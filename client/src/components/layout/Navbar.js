233b6837f14f3139bfd88b291e437ebf05354535
Esther <esther.youri.shin@gmail.com>
2018-11-04 01:29:32 -0700

    added additional dashboard/EditProfile UI and added missing codes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import { logoutUser } from '../../actions/authActions';
+// import { clearCurrentProfile } from '../../actions/profileActions';
 import './Navbar.css';
 import {Link} from 'react-router-dom';
 
