7ef60a0432d0525dc9a6b7d5129f4b0aa3c84270
Rebekah <rebmid@gmail.com>
2019-01-21 16:33:34 -0800

    server changes

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
-const port = 7000;
+const port = process.env.PORT || 7000;
 app.listen(port,() => console.log(`Server running on port ${port}`));
